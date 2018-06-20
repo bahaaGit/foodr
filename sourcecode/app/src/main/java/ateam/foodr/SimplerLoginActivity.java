11f40b26dd1804fbdf5293d0bd01774c2914d63f
Alex Shelley <ashelley@purdue.edu>
2018-06-20 13:16:30 -0400

    Replaced LoginActivity with SimplerLoginActivity.

---
+++ sourcecode/app/src/main/java/ateam/foodr/SimplerLoginActivity.java
@@ -0,0 +1,15 @@
+package ateam.foodr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SimplerLoginActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simpler_login);
+    }
+}
