b86720c23afadbc8c5b55a5d2c189bf5635bb9e1
Alex Shelley <ashelley@purdue.edu>
2018-06-20 13:44:23 -0400

    Started the activity for the list of an owner's restaurants.  The login activity now takes you to that page.

--- sourcecode/app/src/main/java/ateam/foodr/SimplerLoginActivity.java
+++ sourcecode/app/src/main/java/ateam/foodr/SimplerLoginActivity.java
@@ -2,6 +2,9 @@ package ateam.foodr;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+import android.widget.*;
 
 public class SimplerLoginActivity extends AppCompatActivity
 {
@@ -11,5 +14,21 @@ public class SimplerLoginActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_simpler_login);
+    }
+
+    public void onLoginClick(View view)
+    {
+        // TODO: validate credentials before logging in
+
+        // If the user marked themselves as an owner, take them to the owner page
+        Switch ownerToggle = findViewById(R.id.ownerToggle);
+        if (ownerToggle.isChecked())
+        {
+            Intent ownerPageIntent = new Intent(this, OwnerRestaurantListActivity.class);
+            startActivity(ownerPageIntent);
+        }
+
+        // TODO: Do something different if the user isn't
+        // marked as an owner.
     }
 }
