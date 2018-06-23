a0817eb12aa8acf2ae1ea405d7c4765685bdd478
Abdul Aziz Bah <abdulazizbah@ymail.com>
2018-06-23 15:17:07 -0400

    added Recyle view libary set menu UI

---
+++ sourcecode/app/src/main/java/ateam/foodr/UserFoodMenu.java
@@ -0,0 +1,26 @@
+package ateam.foodr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+public class UserFoodMenu extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_food_menu);
+
+        recyclerView = findViewById(R.id.rv);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        RecyclerView.LayoutManager rvLinearLayoutManager = linearLayoutManager;
+
+        recyclerView.setLayoutManager(rvLinearLayoutManager);
+
+    }
+}
