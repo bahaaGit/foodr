93308bd9e02c4eaf380b2eb9b50f1e9e122e201e
Abdul Aziz Bah <abdulazizbah@ymail.com>
2018-06-24 10:36:17 -0400

    add food class recylec view binded with local food object

--- sourcecode/app/src/main/java/ateam/foodr/UserFoodMenu.java
+++ sourcecode/app/src/main/java/ateam/foodr/UserFoodMenu.java
@@ -5,10 +5,13 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
+import java.util.ArrayList;
+
 public class UserFoodMenu extends AppCompatActivity {
 
     RecyclerView recyclerView;
 
+    ArrayList<Food> foodList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,10 +20,20 @@ public class UserFoodMenu extends AppCompatActivity {
 
         recyclerView = findViewById(R.id.rv);
 
+        foodList = new ArrayList<>();
+
+        foodList.add(new Food(R.drawable.topmenubar2,"Food1","23","The is good food"));
+        foodList.add(new Food(R.drawable.topmenubar2,"Food1","23","The is good food"));
+
+
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
         RecyclerView.LayoutManager rvLinearLayoutManager = linearLayoutManager;
 
         recyclerView.setLayoutManager(rvLinearLayoutManager);
+
+        FoodAdapter adapter = new FoodAdapter(this,foodList);
+
+        recyclerView.setAdapter(adapter);
 
     }
 }
