77fa9f2c88c087d3268dda3f64d3ca867628f969
Alex Shelley <ashelley@purdue.edu>
2018-06-25 16:38:04 -0400

    Added an address field to Restaurant
    (cherry picked from commit 4a88e76067bae1ef43c18caa4c9e74e27d9a94bc)

--- sourcecode/app/src/main/java/ateam/foodr/Restaurant.java
+++ sourcecode/app/src/main/java/ateam/foodr/Restaurant.java
@@ -1,5 +1,7 @@
 package ateam.foodr;
 
+import android.location.Address;
+
 import org.jetbrains.annotations.NotNull;
 
 public class Restaurant
@@ -7,24 +9,26 @@ public class Restaurant
     // Boilerplate accessors
     public String getName() { return name; }
     public String getDescription() { return description; }
+    public Address getAddress() {return address;}
     public String getBusinessNumber() {return businessNumber; }
     public String getPhoneNumber() { return phoneNumber; }
 
     // Private fields
     private String name;
     private String description;
+    private Address address;
     private String businessNumber;
     private String phoneNumber;
 
-    // TODO: Location field
     // TODO: Menu array
 
     /** Standard boilerplate constructor. */
-    public Restaurant(String name, String description, String phoneNumber, String businessNumber)
+    public Restaurant(String name, String description, Address address, String phoneNumber, String businessNumber)
     {
         this.name = name;
-        this.businessNumber = businessNumber;
         this.description = description;
+        this.address = address;
+        this.businessNumber = businessNumber;
         this.phoneNumber = phoneNumber;
     }
 
