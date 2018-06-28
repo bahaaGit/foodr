508240ce405cb3a9baa2249d9783f4dcf53a1a44
Alex Shelley <ashelley@purdue.edu>
2018-06-28 17:14:11 -0400

    Wrote the logic for displaying the restaurant list, but it's broken.

--- sourcecode/app/src/main/java/ateam/foodr/OwnerRestaurantListActivity.java
+++ sourcecode/app/src/main/java/ateam/foodr/OwnerRestaurantListActivity.java
@@ -6,10 +6,21 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
 
-public class OwnerRestaurantListActivity extends AppCompatActivity
+import com.google.firebase.auth.*;
+import com.google.firebase.database.*;
+
+import java.util.Map;
+
+public class OwnerRestaurantListActivity extends AppCompatActivity implements ChildEventListener
 {
+    private LinearLayout list;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -18,6 +29,18 @@ public class OwnerRestaurantListActivity extends AppCompatActivity
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // Get the list view
+        View content = findViewById(android.R.id.content);
+        list = content.findViewById(R.id.list);
+
+        // Subscribe to data-changed events
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        String uid = user.getUid();
+        DatabaseReference userDB = FirebaseDatabase.getInstance().getReference().child("Users").child(uid);
+
+        DatabaseReference restaurantList = userDB.child("Restaurants");
+        restaurantList.addChildEventListener(this);
     }
 
     public void onAddButtonClick(View view)
@@ -25,5 +48,38 @@ public class OwnerRestaurantListActivity extends AppCompatActivity
         // Show the create restaurant page
         Intent createIntent = new Intent(OwnerRestaurantListActivity.this, CreateRestaurantActivity.class);
         startActivity(createIntent);
+    }
+
+    public void onCancelled(DatabaseError error)
+    {
+        // TODO: Stuff
+    }
+
+    public void onChildAdded(DataSnapshot snapshot, String prevChildName)
+    {
+        // Add a textview to the list
+        // TODO: Make it a button or something
+        TextView tv = new TextView(this);
+        list.addView(tv);
+
+        // Associate the textview with the Restaurant object
+        Restaurant r = snapshot.getValue(Restaurant.class);
+        tv.setTag(r);
+        tv.setText(r.getName());
+    }
+
+    public void onChildChanged(DataSnapshot snapshot, String prevChildName)
+    {
+
+    }
+
+    public void onChildMoved(DataSnapshot snapshot, String prevChildName)
+    {
+
+    }
+
+    public void onChildRemoved(DataSnapshot snapshot)
+    {
+
     }
 }
