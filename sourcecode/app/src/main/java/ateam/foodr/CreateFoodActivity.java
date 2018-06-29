351005362d52c80af7b43027d9d8191bbdb8a669
Alex Shelley <ashelley@purdue.edu>
2018-06-28 20:47:26 -0400

    Created the references to all the views.

--- sourcecode/app/src/main/java/ateam/foodr/CreateFoodActivity.java
+++ sourcecode/app/src/main/java/ateam/foodr/CreateFoodActivity.java
@@ -2,12 +2,26 @@ package ateam.foodr;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class CreateFoodActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class CreateFoodActivity extends AppCompatActivity
+{
+    @BindView(R.id.nameTextbox)  EditText nameTextbox;
+    @BindView(R.id.priceTextbox) EditText priceTextbox;
+    @BindView(R.id.descTextbox)  EditText descTextbox;
+
+    @BindView(R.id.photoButton)  Button photoButton;
+    @BindView(R.id.createButton) Button createButton;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_food);
+        ButterKnife.bind(this);
     }
 }
